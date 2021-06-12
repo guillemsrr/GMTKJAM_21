af0d760d4b18d6a7e7a846f7a42a155767707052
regorDam <regorm88@gmail.com>
2021-06-12 21:19:09 +0200

    (FEAT) Menu Credits

--- Assets/Scripts/Controllers/MenuController.cs
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField]
+    GameObject _menuPanel;
+    [SerializeField]
+    GameObject _creditsPanel;
 
+    [SerializeField]
+    GameObject _soundTrack;
 
+    private bool _creditsIsOpen = false;
+
+    private void Awake()
+    {
+        _creditsPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(_creditsIsOpen && Input.anyKeyDown)
+        {
+            _soundTrack.SetActive(true);
+            _menuPanel.SetActive(true);
+            _creditsPanel.SetActive(false);
+            _creditsIsOpen = false;
+        }
+    }
     public void OnStartClick()
     {
         LevelManager.Instance.LoadGame();
@@ -14,5 +37,18 @@ public class MenuController : MonoBehaviour
     public void OnExitClick()
     {
         CoreManager.Quit();
+    }
+
+    public void OnClickScores()
+    {
+
+    }
+
+    public void OnClickCredits()
+    {
+        _soundTrack.SetActive(false);
+        _menuPanel.SetActive(false);
+        _creditsPanel.SetActive(true);
+        _creditsIsOpen = true;
     }
 }
