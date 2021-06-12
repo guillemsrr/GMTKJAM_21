c1e261207310df1dc54ea68791950116fdb4303b
Guillem Serra <guillemserralorenz@gmail.com>
2021-06-12 19:59:43 +0200

    base number missions implemented

--- Assets/Scripts/UI/PlayerMissionsHandler.cs
+++ Assets/Scripts/UI/PlayerMissionsHandler.cs
@@ -6,13 +6,20 @@ namespace Controllers
 {
     public class PlayerMissionsHandler: MonoBehaviour
     {
+        private const int BASE_NUMBER_MISSIONS = 1;
+
+        [SerializeField] private Transform _missionContainer;
+        
         private List<SpaceBodyControllerBase.SpaceBodyType> _missionTypes;
         
         public void CreateMissions(int numberMissions)
         {
+            numberMissions += BASE_NUMBER_MISSIONS;
             for (int i = 0; i < numberMissions; i++)
             {
                 _missionTypes.Add(RandomEnum.GetRandomFromEnum<SpaceBodyControllerBase.SpaceBodyType>());
+                
+                //add into container
             }
         }
 
