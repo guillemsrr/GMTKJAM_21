7cd89fe7854685de53744e251bf64cc545c50fbe
regorDam <regorm88@gmail.com>
2021-06-12 19:59:56 +0200

    (FEAT) AudioManager and refactors

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixerGroup _audioMixerGroup;
+    [SerializeField]
+    private AudioSource[] _audioSources = new AudioSource[10];
+
+    public static AudioManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        for(int x = 0; x < _audioSources.Length; x++)
+        {
+            _audioSources[x] = gameObject.AddComponent<AudioSource>() as AudioSource;
+            _audioSources[x].outputAudioMixerGroup = _audioMixerGroup;
+        }
+    }
+
+    public void Play(AudioClip clip)
+    {
+        foreach (var audio in _audioSources)
+        {
+            if (!audio.isPlaying)
+            {
+                audio.clip = clip;
+                audio.Play();
+            }
+            break;            
+        }
+    }
+}
